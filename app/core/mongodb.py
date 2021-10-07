425be138d31810ea3cfa7b70f19817d9a0a00b3a
Kim Kyo <loverkeii@gmail.com>
2021-10-07 19:04:38 +0900

    Added DB Index - users  - email: unique  - phone: unique
    User 생성 시, 중복 에러 처리 추가

--- app/core/mongodb.py
+++ app/core/mongodb.py
@@ -6,5 +6,11 @@ from app.core.config import (
 client = MongoClient(MONGODB_URL)
 db = client[MONGODB_NAME]
 
+def init_index():
+    db.users.create_index([('email', -1)], name='_email_', unique=True)
+    db.users.create_index([('phone', -1)], name='_phone_', unique=True)
+
 def get_db():  
     return db
+
+init_index()
--- app/models/users_repository.py
+++ app/models/users_repository.py
@@ -1,4 +1,6 @@
 from bson.objectid import ObjectId
+from fastapi import HTTPException
+import pymongo
 
 from app.core.mongodb import get_db
 from app.models.interfaces.users_interface import user_create_model
@@ -12,7 +14,13 @@ def find_user_by_id(id: str):
     return convert_id(result)
 
 def add_user(user: user_create_model):
-    result = users.insert_one(user.dict())
-    new_user = find_user_by_id(result.inserted_id)
+    try:
+        result = users.insert_one(user.dict())
+        new_user = find_user_by_id(result.inserted_id)
 
-    return new_user
+        return new_user
+    except pymongo.errors.DuplicateKeyError:
+        print(Exception)
+        raise HTTPException(status_code=400, detail="Duplicated user")
+    except Exception:
+        raise HTTPException(status_code=500, detail="Oops!")
